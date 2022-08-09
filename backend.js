3e9a1b2eddf2baa84ae1cf6f7b5c752639c93326
yuvrajt463 <yuvrajt463@gmail.com>
2022-08-09 17:37:06 -0500

    add delBtn and negBtn functionality

--- backend.js
+++ backend.js
@@ -1,20 +1,20 @@
 let op = "0";
 
-let field1 = " ";
-let field2 = " ";
+let field1 = '';
+let field2 = '';
 
-const numButtons = document.querySelectorAll('.num-btn');
-const oprButtons = document.querySelectorAll('.opr-btn');
+const numButtons = document.querySelectorAll('.num-btn'); //
+const oprButtons = document.querySelectorAll('.opr-btn'); //
 const eqlBtn = document.querySelector('#equal');
-const dotBtn = document.querySelector('#dot');
-const negBtn = document.querySelector('.neg-btn');
-const clrBtn = document.querySelector('#clear');
+const dotBtn = document.querySelector('#dot'); //
+const negBtn = document.querySelector('#negate'); //
+const clrBtn = document.querySelector('#clear'); //
 const delBtn = document.querySelector('#delete');
-let display = document.querySelector('#display');
+let display = document.querySelector('#display-btn'); //
 
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        if(op == "0"){
+        if (op == "0") {
             field1 = field1.concat(button.innerHTML);
             display.innerHTML = field1;
         } else {
@@ -31,28 +31,41 @@ oprButtons.forEach((button) => {
 });
 
 dotBtn.addEventListener('click', () => {
-    if(op == "0" && !field1.includes(".")){
+    if (op == "0" && !field1.includes(".")) {
         field1 = field1.concat(dotBtn.innerHTML);
         display.innerHTML = field1;
-    } else if(op != "0" && !field2.includes(".")) {
+    } else if (op != "0" && !field2.includes(".")) {
         field2 = field2.concat(dotBtn.innerHTML);
         display.innerHTML = field2;
     }
 });
 
 clrBtn.addEventListener('click', () => {
-    field1 = " ";
-    field2 = " ";
+    field1 = '';
+    field2 = '';
     op = "0";
-    display.innerHTML = "0";
+    display.innerHTML = field1;
 });
 
+
 negBtn.addEventListener('click', () => {
-    if(op == "0"){
-        field1 = " " + (0 - parseFloat(field1)).toString();
+    if (op == "0") {
+        field1 = eval(-1 * field1);
+        field1 = "" + field1;
         display.innerHTML = field1;
     } else {
-        field2 = " " + (0 - parseFloat(field2)).toString();
+        field2 = eval(-1 * field2);
+        field2 = "" + field2;
         display.innerHTML = field2;
     }
-})
+});
+
+delBtn.addEventListener('click', () => {
+    if(op == "0"){
+        field1 = field1.slice(0,-1);
+        display.innerHTML = field1;
+    } else {
+        field2 = field2.slice(0,-1);
+        display.innerHTML = field2;
+    }
+});
